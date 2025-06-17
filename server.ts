3d49a159ee4490aac615f793e0220a2dd984b150
Vladimir Georgiev <vladimir.georgiev@apexon.com>
2025-06-17 10:46:15 +0100

    Add backend integration

--- server.ts
+++ server.ts
@@ -15,9 +15,45 @@ const fastify = Fastify({ logger: true });
 
 interface FormData {
   title: string,
-  length: string,
+  length: number,
   genre: string
 }
+
+//init object
+const config = [
+  {
+      title: "Elden Ring",
+      length: 100,
+      genre: "Souls-Like"
+  },
+  {
+      title: "The Witcher 3",
+      length: 200,
+      genre: "Action-RPG"
+  },
+  {
+      title: "Undertale",
+      length: 10,
+      genre: "Indie"
+  },
+  {
+      title: "Final Fantasy 7: Rebirth",
+      length: 68,
+      genre: "JRPG"
+  },
+  {
+      title: "Uncharted: Drake's Fortune",
+      length: 20,
+      genre: "Adventure"
+  }
+]
+const backlogItems: Item[] = [];
+config.forEach(element => {
+  const item = new Item(element);
+  backlogItems.push(item);
+});
+const backlog = new Backlog(backlogItems);
+
 
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, 'frontend'), // where your HTML/JS lives
@@ -30,56 +66,25 @@ const postSchema = {
     required: ['title', 'length', 'genre'],
     properties: {
       title: { type: 'string' },
-      length: { type: 'string' },
+      length: { type: 'number' },
       genre: { type: 'string' },
     }
   }
 };
 
-fastify.post('/submit', { schema: postSchema }, async (request, reply) => {
+fastify.post<{ Body: FormData}>('/submit', { schema: postSchema }, async (request, reply) => {
   return { message: 'Data valid!', data: request.body };
 });
 
-fastify.post<{ Body: FormData}>('/view', async (request, reply) => {
-  const config = [
-      {
-          title: "Elden Ring",
-          length: 100,
-          genre: "Souls-Like"
-      },
-      {
-          title: "The Witcher 3",
-          length: 200,
-          genre: "Action-RPG"
-      },
-      {
-          title: "Undertale",
-          length: 10,
-          genre: "Indie"
-      },
-      {
-          title: "Final Fantasy 7: Rebirth",
-          length: 68,
-          genre: "JRPG"
-      },
-      {
-          title: "Uncharted: Drake's Fortune",
-          length: 20,
-          genre: "Adventure"
-      }
-  ]
-  
-  const backlogItems: Item[] = [];
-  
-  config.forEach(element => {
-      const item = new Item(element);
-      backlogItems.push(item);
-  });
-  
-  const backlog = new Backlog(backlogItems);
+fastify.post('/view', async (request, reply) => {
   const response = backlog.getBacklog().replace(/\n/g, "<br>");
-  
   return { message: 'Data valid!', data: response };
+})
+
+fastify.post<{ Body: FormData }>('/add', { schema: postSchema }, async (request, reply) => {
+  const item = new Item(request.body);
+  backlog.add(item);
+  return { message: 'Object successfully added!'};
 })
 
 const start = async () => {
@@ -93,3 +98,4 @@ const start = async () => {
 };
 
 start();
+
