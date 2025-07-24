bd99575f20d596aab5367521b9895f6759290792
Vladimir Georgiev <vladimir.georgiev@apexon.com>
2025-07-24 15:31:24 +0100

    Back app with JSON file

--- backend/backlog.ts
+++ backend/backlog.ts
@@ -1,35 +1,35 @@
 import Item from "./item";
 
-export default class Backlog{
+export default class Backlog {
 
     private backlog: Item[];
 
-    constructor(backlogItems?: Item[]){
+    constructor(backlogItems?: Item[]) {
         this.backlog = backlogItems || [];
     }
 
     //add item to backlog
-    public add(item: Item){
+    public add(item: Item) {
         this.backlog.push(item);
     }
 
     //remove item from backlog
-    public remove(title: string){
+    public remove(title: string) {
         const item = this.backlog.find((item) => item.getTitle() === title)
         if (item !== undefined) {
             const index = this.backlog.indexOf(item);
-            if (index !== -1){
-                this.backlog.splice(index,1);
+            if (index !== -1) {
+                this.backlog.splice(index, 1);
             }
         }
     }
 
     //update item in backlog
-    public update(title: string, data: Object){
+    public update(title: string, data: Object) {
         const item = this.backlog.find((item) => item.getTitle() === title)
         if (item !== undefined) {
             const index = this.backlog.indexOf(item);
-            if (index !== -1){
+            if (index !== -1) {
                 Object.entries(data).forEach(([key, value]) => {
                     if (value !== undefined && key in this.backlog[index]) {
                         this.backlog[index][key] = value;
@@ -40,10 +40,18 @@ export default class Backlog{
     }
 
     //get backlog items 
-    public getBacklog(){
+    public getBacklogString() {
         let backlogItems: string = ``;
         for (let item of this.backlog) {
             backlogItems += item.toString();
+        }
+        return backlogItems;
+    }
+
+    public getBacklogObject() {
+        let backlogItems: Object[] = [];
+        for (let item of this.backlog) {
+            backlogItems.push(item.toObject());
         }
         return backlogItems;
     }
